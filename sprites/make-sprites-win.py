6adf8bc1c5c848096bd19febab49c1b5644d1bb3
leefige <yifeili3@cs.cmu.edu>
2019-09-10 02:57:09 -0400

    Seems finished.

--- sprites/make-sprites-win.py
+++ sprites/make-sprites-win.py
@@ -2,29 +2,29 @@
 
 #This exists mainly to work around the (many) problems with doing (simple) CLI things on windows. Like lack of shell globbing.
 
-name = "the-planet"
+name = "darkside-of-moon"
 gimp = "C:\\Program Files\\GIMP 2\\bin\\gimp-console-2.10.exe"
 
 import subprocess
 import os
 import shutil
 
-if os.path.exists("../dist/" + name + ".png"):
-	os.unlink("../dist/" + name + ".png")
+# if os.path.exists("../dist/" + name + ".png"):
+# 	os.unlink("../dist/" + name + ".png")
 
-if os.path.exists("../dist/" + name + ".atlas"):
-	os.unlink("../dist/" + name + ".atlas")
+# if os.path.exists("../dist/" + name + ".atlas"):
+# 	os.unlink("../dist/" + name + ".atlas")
 
-if os.path.exists(name):
-	shutil.rmtree(name)
+# if os.path.exists(name):
+# 	shutil.rmtree(name)
 
-subprocess.run([
-	"python",
-	"extract-sprites.py",
-	name + ".list",
-	name,
-	"--gimp", gimp
-], check=True)
+# subprocess.run([
+# 	"python",
+# 	"extract-sprites.py",
+# 	name + ".list",
+# 	name,
+# 	"--gimp", gimp
+# ], check=True)
 
 pngs = []
 for root, dirs, files in os.walk(name):
